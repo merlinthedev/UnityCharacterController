a25fd1a982137a5e7b2ab2fa6dc050c86e7c14d3
merlin <merlijnvan11@gmail.com>
2022-10-11 15:39:46 +0200

    initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+    
+
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+
+    private void Hoverer() {
+        
+    }
+}
